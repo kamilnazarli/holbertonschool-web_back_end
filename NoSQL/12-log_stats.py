1ad09e622a08c6db5ee29a936f7a3c5c665f1c3b
Kamil <neserovk425@gmail.com>
2025-12-21 19:20:24 +0000

    To return schools with specific topic

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+To provide some stats about Nginx logs stored in MongoDB
+'''
+
+import pymongo
+from pymongo import MongoClient
+connection = MongoClient()
+db = connection.logs
+print("{} logs".format{db.nginx.count()})
+print("Methods:")
+method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for i in method:
+    print("method {}: {}".format(i, db.nginx.count({'method': i})))
+print("{} {}".format(db.nginx.count({'method': 'GET'}, {'path' : '/status'})))
