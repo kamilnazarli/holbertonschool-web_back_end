9f054013fcb7a3a998dc61dde1dda29afbce0b82
Kamil <neserovk425@gmail.com>
2025-12-21 18:54:18 +0000

    To return schools with specific topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+'''
+This module returns list of school having specific topic
+'''
+
+def schools_by_topic(mongo_collection, topic):
+    '''
+    To return school that has specific topic
+    '''
+
+    ls = []
+    for school in mongo_collection.find():
+        if topic is in school.get('topics'):
+            ls.append(school)
+    return ls
