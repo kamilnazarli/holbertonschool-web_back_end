fb8d63d7293998c7cddc1b26c5af0aaaa70ed8aa
Kamil <neserovk425@gmail.com>
2025-12-21 19:22:06 +0000

    To return schools with specific topic

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,9 +7,9 @@ import pymongo
 from pymongo import MongoClient
 connection = MongoClient()
 db = connection.logs
-print("{} logs".format{db.nginx.count()})
+print("{} logs".format(db.nginx.count()))
 print("Methods:")
 method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 for i in method:
-    print("method {}: {}".format(i, db.nginx.count({'method': i})))
+    print("method {}: {}".format(i, db.nginx.count(('method': i))))
 print("{} {}".format(db.nginx.count({'method': 'GET'}, {'path' : '/status'})))
