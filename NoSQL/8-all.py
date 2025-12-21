e643004909a193e3d57307dcd9c1a8e55a9e4226
Kamil <neserovk425@gmail.com>
2025-12-21 18:05:23 +0000

    To list all documents

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+'''
+This module creates a function which return list of documents in a collection
+'''
+
 def list_all(mongo_collection):
+    '''
+    This function returns list of documents
+    '''
+
     return db.mongo_collection.find()
