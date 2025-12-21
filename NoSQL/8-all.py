915f916003fa8a3d65ec5d23a46536892dbde1a7
Kamil <neserovk425@gmail.com>
2025-12-21 18:12:02 +0000

    To list all documents

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -7,6 +7,6 @@ def list_all(mongo_collection):
     '''
     This function returns list of documents
     '''
-    if db.mongo_collection.count() == 0:
+    if db.mongo_collection.countDocuments() == 0:
         return []
     return db.mongo_collection.find()
