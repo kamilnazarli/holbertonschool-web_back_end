7a8b80ceae030ad3d4e65e2710a2cd1af29e9428
Kamil <nezerovk425@gmail.com>
2025-12-23 11:43:09 +0000

    To provide some stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python3
+"""
+Provides statistics about Nginx logs stored in MongoDB
+"""
+
 from pymongo import MongoClient
-'''
-To provide some stats about Nginx logs
-'''
 
-client = MongoClient('localhost', 27017)
-db = client.logs
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-clc = db.nginx
-def log_stats(mongo_collection):
-    '''
-    Docstring for log_stats
 
-    :param mongo_collection: takes collection
-    '''
+def log_stats():
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    db = client.logs
+    collection = db.nginx
 
-    print("{} logs".format(clc.count_documents({})))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print("{} logs".format(collection.count_documents({})))
+
     print("Methods:")
     for method in methods:
-        print("method {}: {}".format(method, mongo_collection.count_documents({"method": method})))
-    print("{} status check".format(mongo_collection.count_documents({"method": "GET", "path": "/status"})))
+        print(
+            "\tmethod {}: {}".format(
+                method,
+                collection.count_documents({"method": method})
+            )
+        )
+
+    print(
+        "{} status check".format(
+            collection.count_documents(
+                {"method": "GET", "path": "/status"}
+            )
+        )
+    )
+
+
 if __name__ == "__main__":
-    log_stats(clc)
+    log_stats()
